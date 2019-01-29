78a8ece7c40da5386cd2d535aa9abdeb73bf8a58
ghr85 <ghrenfrew@gmail.com>
2019-01-29 14:06:16 +0000

    Initial commit

---
+++ console.rb
@@ -0,0 +1,8 @@
+# #Codeclan Week 03 Day 02
+# Pizza Shop Codealong
+# Ruby, Databases and their interactions
+# Console file - equivalent to a runner
+# There is no testing for databases on this course - can look into elsewhere.
+
+require_relative('models/property')
+require('pry')
