84c9ec5780191c98cdc08b3ec62dc45345c4160d
ghr85 <ghrenfrew@gmail.com>
2019-01-29 14:49:48 +0000

    Added save functionality to Property Class

--- console.rb
+++ console.rb
@@ -6,3 +6,70 @@
 
 require_relative('models/property')
 require('pry')
+
+
+property_1 = Property.new(
+  {
+'address' => '24 Button Moon Street',
+'value' => 200000,
+'number_of_bedrooms' => 3,
+'year_built' => 1992,
+'status' => 'Buy',
+'square_footage' => 125.5,
+'build_type' => 'Semi-detached'
+}
+)
+
+property_2 = Property.new(
+  {
+'address' => '33 Bit Lane',
+'value' => 150_000, #underscore can be used to separate integers for ease  - RUBY ONLY
+'number_of_bedrooms' => 2,
+'year_built' => 2005,
+'status' => 'Buy',
+'square_footage' => 80.2,
+'build_type' => 'Apartment'
+}
+)
+
+property_3 = Property.new(
+  {
+'address' => '546A Sun Crescent',
+'value' => 600,
+'number_of_bedrooms' => 1,
+'year_built' => 1982,
+'status' => 'Rent',
+'square_footage' => 55,
+'build_type' => 'Apartment'
+}
+)
+
+property_4 = Property.new(
+  {
+'address' => 'Codeclan CEO House',
+'value' => 1_000_000,
+'number_of_bedrooms' => 8,
+'year_built' => 2014,
+'status' => 'Buy',
+'square_footage' => 250.1,
+'build_type' => 'Detached'
+}
+)
+
+property_5 = Property.new(
+  {
+'address' => '34A Slum Village',
+'value' => 250,
+'number_of_bedrooms' => 1,
+'year_built' => 1971,
+'status' => 'Rent',
+'square_footage' => 30,
+'build_type' => 'Apartment'
+}
+)
+
+property_1.save()
+property_2.save()
+property_3.save()
+property_4.save()
+property_5.save()
--- models/property.rb
+++ models/property.rb
@@ -4,20 +4,56 @@
 # Console file - equivalent to a runner
 # There is no testing for databases on this course - can look into elsewhere.
 
+require('PG')
 class Property
 
-attr_accessor :address, :value, :number_of_bedrooms, :year_built, :status, :square_footage, :build_type, :id
+  attr_accessor :address, :value, :number_of_bedrooms, :year_built, :status, :square_footage, :build_type, :id
 
-def initialize(details_hash)
-@address = details_hash['address']
-@value = details_hash['value']
-@number_of_bedrooms = details_hash['number_of_bedrooms']
-@year_built = details_hash['year_built']
-@status = details_hash['status']
-@square_footage = details_hash['square_footage']
-@build_type = details_hash['build_type']
-@id = details_hash['id'].to_i if details_hash['id'] #will assign this id if it has one, not required to instantiate
-end
+  def initialize(details_hash)
+    @address = details_hash['address']
+    @value = details_hash['value']
+    @number_of_bedrooms = details_hash['number_of_bedrooms']
+    @year_built = details_hash['year_built']
+    @status = details_hash['status']
+    @square_footage = details_hash['square_footage']
+    @build_type = details_hash['build_type']
+    @id = details_hash['id'].to_i if details_hash['id'] #will assign this id if it has one, not required to instantiate
+  end
+
+
+  def save
+    #connect to DB. Ask PG to connect, return a DB object.
+    db = PG.connect( { dbname: 'properties', host: 'localhost'})
+
+    #Define SQL - USE DOUBLE QUOTES HERE AS SQL WITHIN WILL USE SINGLE AROUND INTERPOLATED STRINGS
+    sql = "INSERT INTO properties (
+    address,
+    value,
+    number_of_bedrooms,
+    year_built,
+    status,
+    square_footage,
+    build_type
+    ) VALUES (
+      $1,
+      $2,
+      $3,
+      $4,
+      $5,
+      $6,
+      $7
+    )
+    RETURNING id" #get id column from DB so that you can save on object
+    values = [@address, @value, @number_of_bedrooms, @year_built, @status, @square_footage, @build_type] #order of array needs to be in order of dollar values
+    db.prepare("save_as", sql ) #PREPARED STATEMENT, BREAK SQL INJECTION ATTACK
+    #execute SQL on DB object. SQL instruction  passed as a parameter and executed on DB objet.
+    # db.exec(sql) - no longer required
+    @id = db.exec_prepared("save_as", values)[0]['id'].to_i #PG gem gives you everything in an array RETURNING id. PG gem gives everything as string.
+    #combining DB writing and id returning into one statement
+    #Close DB Connection
+
+    db.close()
+  end
 
 
 end   #class end
